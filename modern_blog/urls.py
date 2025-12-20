cb8acc9fc8529b21a653d58f9f4a223ae3f43ddd
SALEH ALGABRI <s96557867@gmail.com>
2025-12-20 16:56:14 +0300

    Allowing users to upload files and images

--- modern_blog/urls.py
+++ modern_blog/urls.py
@@ -5,11 +5,20 @@ from django.contrib.auth.views import LoginView
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from ckeditor_uploader import views as ckeditor_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('ckeditor/upload/', login_required(ckeditor_views.upload), name='ckeditor_upload'),
+    path('ckeditor/browse/', never_cache(login_required(ckeditor_views.browse)), name='ckeditor_browse'),
+
+    # إبقاء هذا السطر كاحتياط لبقية الملفات (JS/CSS)
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
